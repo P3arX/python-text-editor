8d2d5bb818380e95a30f9618b81feafeec2815ef
Alex Kollár <P3arX@users.noreply.github.com>
2020-12-02 07:55:14 +0100

    Main window creation
    Set up the base window for the application.

--- Text Editor/Text Editor/Text_Editor.py
+++ Text Editor/Text Editor/Text_Editor.py
@@ -1,1 +1,10 @@
+import tkinter as tk
 
+class Application(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master);
+        root.title("Text Editor")
+
+root = tk.Tk()
+myapp = Application(root)
+myapp.mainloop()
