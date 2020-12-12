f1c1c440ac3f9dad514401309cd7faf9b23458e8
Alex Kollár <P3arX@users.noreply.github.com>
2020-12-12 16:53:53 +0100

    New file function
    +Added new file function
    *minor adjustments, like "New" -> "New file"

--- Text Editor/Text Editor/Text_Editor.py
+++ Text Editor/Text Editor/Text_Editor.py
@@ -1,19 +1,27 @@
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 import os
 
 root = tk.Tk()
 root.title("Text Editor")
 
+def NewFile():
+    savingcurrent = messagebox.askquestion(title="Unsaved file", message="You have unsaved work. Do you want to save it?")
+    if savingcurrent == "yes":
+        SaveFile()
+            
+    editor.delete(1.0, tk.END)
+
 def OpenFile():
     filelocation = filedialog.askopenfilename(initialdir="/", title="Select a file", filetypes=(("Texts", "*.txt"), ("All files", "*.*")))
     file = open(filelocation, "r")
+    editor.delete(1.0, tk.END)
     editor.insert(tk.END, file.readlines())
 
 def SaveFile():
     file = filedialog.asksaveasfile(filetypes=[("Text (.txt)", "*.txt")])
     if file is None:
-        return
+        return None
     file.write(editor.get("0.0", tk.END))
     file.close()
     
@@ -30,8 +38,9 @@ root.config(menu = menubar)
 
 #FILE MENUBAR
 fileMenu = tk.Menu(menubar)
-fileMenu.add_command(label="Open", command=OpenFile)
-fileMenu.add_command(label="Save", command=SaveFile)
+fileMenu.add_command(label="New file", command=NewFile)
+fileMenu.add_command(label="Open file", command=OpenFile)
+fileMenu.add_command(label="Save file", command=SaveFile)
 menubar.add_cascade(label="File", menu=fileMenu)
 
 root.mainloop()
